afa917977a6025cc7bf15667357d8d7e392ec8dc
Andrew Bunday <andrew.bunday@baseblack.com>
2011-04-13 18:09:56 +0100

    Removing superfluous files from the package.

--- shotgun/api/keys.py
+++
@@ -1,8 +0,0 @@
-url = "https://baseblack.shotgunstudio.com"
-
-keys = {}
-keys['DailiesPlaylist'] = "84d454053f918778282f5bb6b707e3765bb2a7cc"
-keys['BaselightSync'] = "fb6179c6e2db20d37c97a8bb4ba242202ee72427"
-keys['EventProcessor'] = "48f6f6cece59db2e4216ed6c1d39228b34a7f81b"
-keys['mkQuicktime'] = "b95788fa77d07843e6600d51902f63708773323c"
-keys['sgUpdateQt'] = "4f47bbbf75483a0fa0b4755f9698d13e6454e4b7"
